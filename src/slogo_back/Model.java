4fc5b02d7f0672146f3797bdbe2271546a239b10
bojia-chen <bojia.chen@duke.edu>
2015-02-20 00:07:19 -0500

    Foundation of Model, Command, Parser, and Manager

--- src/slogo_back/Model.java
+++ src/slogo_back/Model.java
@@ -1,5 +1,34 @@
 package slogo_back;
 
+import java.lang.reflect.Method;
+
+import slogo_front.Manager;
+
 public class Model {
 
+	private Manager mgr;
+	
+	public Model(){
+		
+	}
+	
+	public void setManager(Manager manager){
+		mgr = manager;
+	}
+	
+	public Number toFront(String cmd, Number param){
+		Method toRun;
+		try {
+			toRun = Manager.class.getMethod(cmd, Number.class);
+			return (Number) toRun.invoke(mgr, param);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public Number toBack(String cmds) {
+		Parser parser = new Parser();
+		return parser.parse(cmds);
+	}
 }
