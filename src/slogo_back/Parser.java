f1b16f640f3bdfbd45b73194ab5e23368fd6a364
Janan Zhu <janan.zhu@duke.edu>
2015-02-20 14:29:40 -0500

    basic constant parser

--- src/slogo_back/Parser.java
+++ src/slogo_back/Parser.java
@@ -1,16 +1,25 @@
 package slogo_back;
 
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
     private List<String> myValidCommands;
+    private ResourceBundle myResourceBundle;
+    private Pattern constantPattern;
     
     public Parser(Model currentModel){
-        myValidCommands = currentModel.getValidCommands();
+//        myValidCommands = currentModel.getValidCommands();
+        myResourceBundle = ResourceBundle.getBundle("resources/languages/Syntax");
+        constantPattern = Pattern.compile(myResourceBundle.getString("HeadConstant"));
+
     }
     
-    public List<Double> parseRunCommands(Stack<String> commands){
+    public Double parseRunCommands(Stack<String> commands){
+        
         String command = commands.pop();
         if(myValidCommands.contains(command)){
             
@@ -19,6 +28,25 @@ public class Parser {
         return null;
     }
     
+    public List<Double> parseInput(String input){
+        Matcher m =constantPattern.matcher(input);
+        if(m.find()){
+            input = input.split(constantPattern.toString())[1];
+            System.out.println(input.split
+                               (myResourceBundle.getString("LeadingWhitespace"))[1]);
+            System.out.println(m.group(0));
+        }
+        return null;
+    }
     
+    private Stack<String> inputToTokens(String input){
+        return null;
+    }
+    
+    public static void main(String[] args){
+        String test = "50    \n fadfasdjlf 3453 2343 gdsdf";
+        Parser testParser = new Parser(null);
+        testParser.parseInput(test);
+    }
     
 }
