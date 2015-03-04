291d20897d7e0ff4accdee3b38233587318df6b0
Janan Zhu <janan.zhu@duke.edu>
2015-03-04 14:20:55 -0500

    fixed handling of parameters to For and DoTimes as ISyntaxNodes

--- src/commands/DoTimes.java
+++ src/commands/DoTimes.java
@@ -5,41 +5,45 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
 import java.util.ResourceBundle;
+import slogo_back.ISyntaxNode;
 import slogo_back.Model;
 
 public class DoTimes extends Command {
-    
+
     private String myVariable;
     private int myLimit;
 
     private final static int numParams = 2;
-    
+
     public DoTimes (Queue<String> cmdQueue,
                     Model model,
                     Map<String, Double> variableMap) {
         super(cmdQueue, model, numParams, variableMap);
-        String parameterString = cmdQueue.peek();
-        String[] parameters = parameterString.split("\\s*");
-        if(parameters.length != numParams){
-            throw new InvalidParameterException();
-        }
-        if(parameters[0].matches(ResourceBundle.getBundle("resources/languages/Syntax").getString("HeadVariable"))
-                && parameters[1].matches(ResourceBundle.getBundle("resources/languages/Syntax").getString("HeadConstant"))){
-                myVariable = parameters[0].substring(1, parameters[0].length());
-                myLimit = (int) Double.parseDouble(parameters[1]);
-                Map<String,Double> newVariableMap = new HashMap<String,Double>();
-                for(String key: myVariableMap.keySet()){
-                    newVariableMap.put(key,myVariableMap.get(key));
-                }
-                myVariableMap.put(myVariable, (double) 1);
-            }else{
-                throw new InvalidParameterException();
+    }
+
+    @Override
+    protected ISyntaxNode[] defineParams(int numParams){
+        ISyntaxNode[] returnParams = new ISyntaxNode[numParams];
+        String parameterString = myCmds.poll();
+        Queue<String> parameterQueue = myParser.inputTokenizer(parameterString);
+        myVariable = parameterQueue.poll();
+        if(myVariable.matches(ResourceBundle.getBundle("resources/languages/Syntax").getString("HeadVariable"))){
+            for(int i=0; i< numParams-1;i++){
+                returnParams[i] = myParser.buildParseTree(parameterQueue, myVariableMap);
             }
+            //TODO:Throw exception for nonstandard number of parameters here, ie. >4
+        } else{
+            throw new InvalidParameterException(parameterString + " is invalid list of For parameters");
+        }
+
+        returnParams[numParams-1] = myParser.buildParseTree(myCmds, myVariableMap);
+        return returnParams;
     }
 
     @Override
     public double getValue () {
         double returnValue = 0;
+        myLimit = (int) myParams[0].getValue();
         for(double i = 1; i<=myLimit; i++){
             myVariableMap.put(myVariable, i);
             returnValue = myParams[1].getValue();
--- src/slogo_back/Parser.java
+++ src/slogo_back/Parser.java
@@ -40,7 +40,6 @@ public class Parser {
 
     public ISyntaxNode buildParseTree(Queue<String> tokenQueue, Map<String,Double> variableMap){
         String token = tokenQueue.poll();
-        System.out.println("Token is " + token);
         Matcher listMatch = listPattern.matcher(token);
         ISyntaxNode node;
         if(myValidCommands.contains(token)){
@@ -62,7 +61,6 @@ public class Parser {
         List<ISyntaxNode> commands = new ArrayList<ISyntaxNode>();
         while(!tokens.isEmpty()){
             if(myValidCommands.contains(tokens.peek())){
-                System.out.println("parseInput calling");
                 ISyntaxNode command = buildParseTree(tokens,myModel.getVarMap());
                 commands.add(command);
             }else{
@@ -72,8 +70,7 @@ public class Parser {
         return commands;
     }
 
-    private Queue<String> inputTokenizer(String input){
-        System.out.println("making tokens");
+    public Queue<String> inputTokenizer(String input){
         Queue<String> tokenQueue = new LinkedList<String>();
         while(!input.matches(whitespacePattern.toString())){
             Matcher constantMatch =constantPattern.matcher(input);
