a7eef75657cafbbb68853071877786dfc1e9e8a5
bojia-chen <bojia.chen@duke.edu>
2015-03-07 01:08:57 -0500

    Implemented Recursion!

--- src/commands/UserInstruction.java
+++ src/commands/UserInstruction.java
@@ -9,6 +9,7 @@ import slogo_back.Model;
 public class UserInstruction extends Command {
 	
 	private final static int INITIAL_NUMBER_PARAMETERS = 1;
+	private UserInstruction myParentInstruction;
 	private ISyntaxNode mySynTree;
 
 	
@@ -18,37 +19,45 @@ public class UserInstruction extends Command {
 		super(cmdQueue, model, variableMap);
 	}
 	
-	//define local variables before creating new instance
-	public Command cloneCommand(Queue<String> cmdQueue) {
-		return new UserInstruction(cmdQueue, myModel, myVarMap, myVarMap.size(), mySynTree);
+	protected void postConstructProcessing(String instName) {
+		myModel.setUsrCmd(instName, this);
+		myParams = super.defineParams(INITIAL_NUMBER_PARAMETERS);
+		mySynTree = myParams[0];
+		//System.out.println("Updated command map");
 	}
 	
+	//define local variables before creating new instance
+	public Command cloneCommand(Queue<String> cmdQueue) {
+		return new UserInstruction(cmdQueue, myVarMap.size(), this);
+	}
+	
+	public UserInstruction(Queue<String> cmdQueue, int numParams, UserInstruction parent) {
+		super(cmdQueue, parent.myModel, numParams, parent.myVarMap);
+		myParentInstruction = parent;
+	}
 	
 	//Methods for running instances
 	public UserInstruction(Queue<String> cmdQueue, Model model,
 			Map<String, Double> variableMap, int numParams, ISyntaxNode synTree) {
 		super(cmdQueue, model, numParams, variableMap);
 		mySynTree = synTree;
-		int paramInd = 0;
-		for (String var : myVarMap.keySet())
-			myVarMap.put(var, myParams[paramInd++].getValue());
-		for (String var : myVarMap.keySet())
-		    System.out.println(var + " : " + myVarMap.get(var));
 	}
 	
 	@Override
 	public double getValue() {
+		mySynTree = myParentInstruction.mySynTree;
+		int paramInd = 0;
+		for (String var : myVarMap.keySet())
+			myVarMap.put(var, myParams[paramInd++].getValue());
+		
+		System.out.println("Setting variables...");
+		for (String var : myVarMap.keySet())
+		    System.out.println(var + " : " + myVarMap.get(var));
+		System.out.println("Done variables\n\n");
 		setLocalVars(myVarMap);
 		double retValue = mySynTree.getValue();
 		clearLocalVars(myVarMap);
 		return retValue;
-	}
-	
-	protected void postConstructProcessing(String instName) {
-		myModel.setUsrCmd(instName, this);
-		myParams = super.defineParams(INITIAL_NUMBER_PARAMETERS);
-		mySynTree = myParams[0];
-		//System.out.println("Updated command map");
 	}
 
 }
